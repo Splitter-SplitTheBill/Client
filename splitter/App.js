20332f4cd5084734ead2ecdf2c26f5ac07952e9d
fadhilahrayafi <fadhilahrayafi@gmail.com>
2020-03-20 22:33:24 +0700

    almost finnish display on login and register

--- splitter/App.js
+++ splitter/App.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 // import { Provider } from 'react-redux'
 // import store from './store'
-import { HomeScreen } from './screens'
+import {
+  HomeScreen,
+  LoginScreen,
+  RegisterScreen,
+  RegistAddScreen
+} from "./screens";
 
-const Stack = createStackNavigator()
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     // <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{
-            headerShown: false,
-            unmountInactiveRoutes: true
-          }}>
-            <Stack.Screen name="Home" component={HomeScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          unmountInactiveRoutes: true
+        }}
+      >
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="RegistAdd" component={RegistAddScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
     // </Provider>
   );
 }
-
---
+++ splitter/screens/RegisterScreen.jsx
@@ -0,0 +1,122 @@
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity
+} from "react-native";
+import logoImage from "../assets/logo.jpg";
+
+export default function RegisterScreen(props) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.boxLogo}>
+        <Image style={styles.imageLogo} source={logoImage} />
+        <Text style={styles.textLogo}>Splitter</Text>
+      </View>
+      <View style={styles.boxLogin}>
+        <Text style={styles.loginText}>Register</Text>
+        <Text style={styles.textInput}>Username</Text>
+        <TextInput
+          editable
+          maxLength={40}
+          //   onChangeText={text => setName(text)}
+          //   value={name}
+          style={styles.inputLogin}
+        ></TextInput>
+        <Text style={styles.textInput}>Password</Text>
+        <TextInput
+          editable
+          maxLength={40}
+          //   onChangeText={text => setName(text)}
+          //   value={name}
+          style={styles.inputLogin}
+        ></TextInput>
+        <Text style={styles.textInput}>Email</Text>
+        <TextInput
+          editable
+          maxLength={40}
+          //   onChangeText={text => setName(text)}
+          //   value={name}
+          style={styles.inputLogin}
+        ></TextInput>
+        <View style={styles.buttonLogin}>
+          <Button
+            title="Register"
+            onPress={() => props.navigation.navigate("Home")}
+            color="#6597A0"
+          />
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  textLogo: {
+    color: "white",
+    fontSize: 50,
+    marginLeft: 10
+  },
+  imageLogo: {
+    width: 100,
+    height: 100,
+    borderColor: "white",
+    borderWidth: 2
+  },
+  loginText: {
+    fontSize: 20,
+    fontWeight: "normal",
+    textAlign: "center"
+  },
+  boxLogin: {
+    backgroundColor: "white",
+    padding: 15,
+    shadowColor: "black",
+    shadowOpacity: 0.5,
+    width: 250,
+    zIndex: 5,
+    shadowRadius: 6,
+    elevation: 2,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  inputLogin: {
+    borderBottomColor: "#6597A0",
+    borderBottomWidth: 2,
+    marginBottom: 5
+  },
+  textInput: {
+    color: "#6597A0",
+    marginBottom: 5,
+    marginTop: 5,
+    marginRight: "auto"
+  },
+  boxLogo: {
+    paddingTop: 100,
+    paddingBottom: 50,
+    backgroundColor: "#6597A0",
+    width: "100%",
+    zIndex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  buttonLogin: {
+    width: 80,
+    shadowRadius: 3,
+    elevation: 2,
+    marginTop: 5
+  },
+  textRegister: {
+    marginTop: 20,
+    fontSize: 15
+  }
+});
